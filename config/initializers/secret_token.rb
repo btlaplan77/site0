50d7dc794d7c899a71b2c3ee3a48a73c3b1e1ba6
tyler-rand <tprand@syr.edu>
2013-11-07 21:11:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trand::Application.config.secret_key_base = '7e6718348496a748f08f0d35fc2af1273cdc0afbd5456ef856f51425ff48ba99b73099e01463ab0e8e23dd3d635be0c685bb0bc747fb37f21b69a7e4e873b819'
